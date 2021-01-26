e56554e61d0e8805e9abd0d09ab1d10abd962b9b
Xavier <xavier4-10@hotmail.com>
2021-01-26 11:38:17 -0500

    Ajout de cors

--- WebAPI/API/App_Start/WebApiConfig.cs
+++ WebAPI/API/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
 
@@ -12,6 +13,8 @@ namespace API
     {
         public static void Register(HttpConfiguration config)
         {
+            var cors = new EnableCorsAttribute("https://e4.projet.college-em.info/,https://test.e4.projet.college-em.info/,https://localhost:4200", "*", "*");
+            config.EnableCors(cors);
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
