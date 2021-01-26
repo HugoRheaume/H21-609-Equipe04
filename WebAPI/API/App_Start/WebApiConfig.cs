fb319a36b0c13bba908b09296ac3bedc28623d50
Xavier <xavier4-10@hotmail.com>
2021-01-26 12:21:10 -0500

    Serialization de PascalCase en camelCase

--- WebAPI/API/App_Start/WebApiConfig.cs
+++ WebAPI/API/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web.Http;
 using System.Web.Http.Cors;
 using Microsoft.Owin.Security.OAuth;
@@ -19,6 +20,7 @@ namespace API
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
 
             // Web API routes
             config.MapHttpAttributeRoutes();
