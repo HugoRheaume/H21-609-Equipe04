7d194a3a429c94c1b8653f4db315fc699f6a7b7c
Xavier <xavier4-10@hotmail.com>
2021-01-27 14:01:52 -0500

    Ajout de la création de quiz

---
+++ WebAPI/API/Controllers/QuizController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using API.Models;
+using Microsoft.AspNet.Identity;
+
+namespace API.Controllers
+{
+    public class QuizController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Create(QuizRequestDTO quizRequestDto)
+        {
+            // Enlever le "1" lorsque les user seront implémentés
+            string userId = User.Identity.GetUserId() ?? "1";
+            Quiz quiz = new Quiz()
+            {
+                Title = quizRequestDto.Title,
+                Description = quizRequestDto.Description,
+                IsPublic = quizRequestDto.IsPublic,
+                OwnerId = userId
+            };
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+                if (context.ListQuiz.Any(q => q.Title == quizRequestDto.Title && q.OwnerId == userId))
+                {
+                    // Envoie un message pour que l'utilisateur confirme
+                    HttpResponseMessage message = new HttpResponseMessage(HttpStatusCode.Accepted)
+                    {
+                        ReasonPhrase = "Quiz title already exist"
+                    };
+
+                    return ResponseMessage(message);
+                }
+
+                context.ListQuiz.Add(quiz);
+                context.SaveChanges();
+            }
+
+            QuizResponseDTO response = new QuizResponseDTO()
+            {
+                Id = quiz.Id,
+                Author = User.Identity.Name ?? "Nobody",
+                Description = quiz.Description,
+                IsPublic = quiz.IsPublic,
+                ShareCode = quiz.ShareCode,
+                Title = quiz.Title
+            };
+            
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public IHttpActionResult ConfirmCreate(QuizRequestDTO quizRequestDto)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+
+            string userId = User.Identity.GetUserId() ?? "1";
+            Quiz quiz = new Quiz()
+            {
+                Title = quizRequestDto.Title,
+                Description = quizRequestDto.Description,
+                IsPublic = quizRequestDto.IsPublic,
+                OwnerId = userId
+            };
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+                context.ListQuiz.Add(quiz);
+                context.SaveChanges();
+            }
+
+            QuizResponseDTO response = new QuizResponseDTO()
+            {
+                Id = quiz.Id,
+                Author = User.Identity.Name ?? "Nobody",
+                Description = quiz.Description,
+                IsPublic = quiz.IsPublic,
+                ShareCode = quiz.ShareCode,
+                Title = quiz.Title
+            };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ WebAPI/API/Models/Quiz/QuizResponseDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Common.CommandTrees;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class QuizResponseDTO
+    {
+        public int Id { get; set; }
+
+        public string Author { get; set; }
+
+        public string Title { get; set; }
+
+        public bool IsPublic { get; set; }
+
+        public string Description { get; set; }
+
+        public string ShareCode { get; set; }
+    }
+}
