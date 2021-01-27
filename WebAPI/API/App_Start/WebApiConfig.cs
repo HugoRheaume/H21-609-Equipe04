ca625daeb5abd61b60b969a60dc8ba1a08e5fcbc
Hugo <hugo.rheaume@hotmail.com>
2021-01-27 09:45:30 -0500

    {controller}/{action}/{id}

--- WebAPI/API/App_Start/WebApiConfig.cs
+++ WebAPI/API/App_Start/WebApiConfig.cs
@@ -29,7 +29,7 @@ namespace API
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
