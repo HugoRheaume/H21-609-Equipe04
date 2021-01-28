5b2a6799fe22bd0981ecf401ad048984892db768
Hugo <hugo.rheaume@hotmail.com>
2021-01-28 17:54:53 -0500

    Ajout Service pour QuizController

---
+++ WebAPI/API/Service/QuizService.cs
@@ -0,0 +1,39 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Web;
+
+namespace API.Service
+{
+    public class QuizService : BaseService
+    {
+        public QuizService(ApplicationDbContext db) : base(db) { }
+
+
+        public QuizResponseDTO CreateQuiz(Quiz quiz, string username)
+        {
+            Quiz newQuiz = db.ListQuiz.Add(quiz);
+            db.SaveChanges();
+
+
+            QuizResponseDTO response = new QuizResponseDTO()
+            {
+                Id = newQuiz.Id,
+                Author = username ?? "Nobody",
+                Description = newQuiz.Description,
+                IsPublic = newQuiz.IsPublic,
+                ShareCode = newQuiz.ShareCode,
+                Title = newQuiz.Title
+            };
+            return response;
+        }
+        public bool QuizCheck(string userId, string quizTitle)
+        {
+            return db.ListQuiz.Any(q => q.Title == quizTitle && q.OwnerId == userId);
+        }
+    }
+}
---
+++ WebAPI/API/Validation/ModelValidationAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace API.Validation
+{
+    public class ModelValidationAttribute: ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            if (!actionContext.ModelState.IsValid)
+            {
+                actionContext.Response = new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
