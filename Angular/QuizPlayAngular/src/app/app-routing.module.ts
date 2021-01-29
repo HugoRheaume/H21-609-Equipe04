7b0d1e5b85a747f941e620caa57d4b96d637b83c
Hugo <hugo.rheaume@hotmail.com>
2021-01-29 15:41:28 -0500

    + (rebase master)

--- Angular/QuizPlayAngular/src/app/app-routing.module.ts
+++ Angular/QuizPlayAngular/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
-import { AlphanumericCodeComponent } from './../components/alphanumeric-code/alphanumeric-code.component';
 import { CreateTrueOrFalseQuestion } from './../components/create-trueorfalse-question/create-trueorfalse-question.component';
 import { CreateQuizComponent } from 'src/components/create-quiz/create-quiz.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QuizQuestionListComponent } from './components/quiz-question-list/quiz-question-list.component';
 
 const routes: Routes = [
   { path: '', component: CreateQuizComponent },
-  { path: 'CreatQuestionTrueOrFalse', component: CreateTrueOrFalseQuestion},
-  {path: 'CreateQuiz/:code', component: AlphanumericCodeComponent}];
+  { path: 'CreatQuestionTrueOrFalse', component: CreateTrueOrFalseQuestion },
+  { path: 'quiz/:quizId', component: QuizQuestionListComponent },
+  { path: 'quiz', component: CreateQuizComponent }
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
